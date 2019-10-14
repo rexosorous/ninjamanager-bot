39137896b6287274758c25778890dc840afd21e2
J-Zach Loke <jzloke@gmail.com>
2019-10-13 23:34:46 -0700

    did some bugfixin, but i need to migrate to selenium

--- nmbot.py
+++ nmbot.py
@@ -1,24 +1,19 @@
 import requests
 import os
 import json
+import traceback
 from random import uniform
 from time import sleep
 
 
 # TO DO
+# MIGREATE TO SELENIUM
 # gold gain from world grinding
 # legendary weapon grinding
 
 
 
 
-
-
-with open('headers.json', 'r') as file:
-    HEADERS = json.load(file)
-
-with open('cookies.json', 'r') as file:
-    COOKIES = json.load(file)
 
 WORLD_MISSION_URL = 'https://www.ninjamanager.com/world/area/anbu-hideout/mission/6'
 
@@ -36,17 +31,19 @@ class MaxChallenges(Exception):
 
 
 class NMBot():
-    def __init__():
+    def __init__(self, headers, cookies):
         self.session = requests.Session()
+
+        self.headers = headers
+        self.cookies = cookies
 
         self.arena_energy = True
         self.world_energy = True
 
-        self.team_blacklist = set()
+        self.team_blacklist = {'965'} # 965 = my own team
 
         self.arena_successes = 0
         self.arena_losses = 0
-        self.arena_rating = 0
 
         self.world_successes = 0
         self.world_losses = 0
@@ -58,30 +55,28 @@ class NMBot():
 
 
 
-    def execute():
+    def execute(self):
         # maion logic
-        loop_count = 1
+        loop_count = 0
 
         try:
             while True:
                 self.check_energy()
 
                 if loop_count % 5 == 0:
-                    self.team_blacklist = set()
+                    self.team_blacklist = {'965'}
 
-                if arena_energy or world_energy:
-                    self.log('LOOP #' + loop_count + '\n')
+                if self.arena_energy or self.world_energy:
+                    self.log('LOOP #' + str(loop_count) + '\n')
 
-                    if arena_energy:
-                        self.log('ARENA')
-                        self.arena_actions()
-                        self.log('\n')
-                        sleep(rng(300, 600)) # 5 to 10 minutes
-                    else:
-                        self.log('ARENA out of energy' + '\n')
+                    # if self.arena_energy:
+                    #     self.arena_actions()
+                    #     self.log('\n')
+                    #     sleep(rng(300, 600)) # 5 to 10 minutes
+                    # else:
+                    #     self.log('ARENA out of energy' + '\n')
 
-                    if world_energy:
-                        self.log('WORLD')
+                    if self.world_energy:
                         self.world_actions()
                         self.log('\n')
                         sleep(rng(300, 600)) # 5 to 10 minutes
@@ -93,30 +88,30 @@ class NMBot():
                 else:
                     sleep(rng(1200, 1800)) # 20 to 30 minutes
         except Exception as e:
-            self.log(e)
+            self.log('\n\n\n\n\n\n')
+            self.log('%s' % e)
+            self.log(traceback.format_exc())
             self.logger.close()
             with open('summary.txt', 'w+') as file:
-                file.write('Total Loops:  ' + loop_count + '\n' +
-                         '\nArena Wins:   ' + self.arena_successes +
-                         '\nArena Losses: ' + self.arena_losses +
-                         '\nRating Delta: ' + self.arena_rating + '\n' +
-                         '\nWorld Wins:   ' + self.world_successes +
-                         '\nWorld Losses: ' + self.world_losses +
-                         '\nItems Gained: ' + self.item_successes)
+                file.write('Total Loops:  ' + str(loop_count) + '\n' +
+                         '\nArena Wins:   ' + str(self.arena_successes) +
+                         '\nArena Losses: ' + str(self.arena_losses) +
+                         '\nWorld Wins:   ' + str(self.world_successes) +
+                         '\nWorld Losses: ' + str(self.world_losses) +
+                         '\nItems Gained: ' + str(self.item_successes))
 
 
 
 
 
     # ARENA
-    def arena_actions():
+    def arena_actions(self):
         # all the things we want to do in the arena screen
         try:
             challengers = self.get_challengers()
             for team in challengers:
-                sleep(rng(0.5, 3))
+                sleep(rng(1, 3))
                 self.challenge(team)
-            self.log('finished challenging teams')
         except FileNotFoundError:
             self.log('ERROR READING FROM response.txt')
         except OutOfEnergy:
@@ -124,9 +119,9 @@ class NMBot():
 
 
 
-    def get_challengers() -> set(str):
+    def get_challengers(self) -> {str}:
         # gets a list of rematch challengers
-        raw = self.session.get('http://www.ninjamanager.com/arena', headers=HEADERS, cookies=COOKIES)
+        raw = self.session.get('https://www.ninjamanager.com/arena', headers=self.headers, cookies=self.cookies)
         challengers = set()
 
         with open('response.txt', 'w+', encoding='utf-8') as file:
@@ -144,9 +139,9 @@ class NMBot():
 
 
 
-    def challenge(team_id: str):
+    def challenge(self, team_id: str):
         # challenges a team
-        response = self.session.post('http://www.ninjamanager.com/ajax/challengeTeam', headers=HEADERS, cookies=COOKIES, data={'TeamID': team_id, 'SkipBattle': 'true'})
+        response = self.session.post('https://www.ninjamanager.com/ajax/challengeTeam', headers=self.headers, cookies=self.cookies, data={'TeamID': team_id, 'SkipBattle': 'true'})
 
         if 'Not enough energy!' in response.text:
             raise OutOfEnergy
@@ -155,48 +150,48 @@ class NMBot():
             self.team_blacklist.add(team_id)
             raise MaxChallenges
 
-        info = json.load(response.text)
+        with open('arena_debug.txt', 'w+') as file:
+            file.write(response.text)
 
-        self.log('challenged team ' + team_id)
+        info = response.json()
 
         if '-result-win' in info['challengeBox']:
             self.arena_successes += 1
-            self.log('arena won')
+            self.log('won  against team ' + team_id)
         elif '-result-loss' in info['challengeBox']:
             self.arena_losses += 1
-            self.log('arena lost')
-
-        self.arena_rating += int(info['mission']['ChallengeBox']['Rating'])
-        self.log('rating gain = ' + info['mission']['ChallengeBox']['Rating'])
+            self.log('lost against team ' + team_id)
 
 
 
 
 
     # WORLD
-    def world_actions():
+    def world_actions(self):
         # all the things we want to do in the world screen
         try:
             info = self.do_mission(WORLD_MISSION_URL)
-            self.log('finished world mission(s)')
         except OutOfEnergy:
             self.log('OUT OF WORLD ENERGY')
 
 
 
-    def do_mission(url: str):
+    def do_mission(self, url: str):
         # executes world mission
-        response = self.session.get(url, headers=HEADERS, cookies=COOKIES)
+        response = self.session.get(url, headers=self.headers, cookies=self.cookies)
 
         if 'Not enough world energy!' in response.text:
             raise OutOfEnergy
 
-        info = json.load(response.text)
+        with open('world_debug.txt', 'w+') as file:
+            file.write(response.text)
+
+        info = response.json()
 
         if r'pm-battle-matchup__title\">Defeat<\/div>' in info['content']:
             self.world_successes += 1
             self.log('world won')
-        elif r'pm-battle-matchup__title\">Victory<\/div>' in info['content']
+        elif r'pm-battle-matchup__title\">Victory<\/div>' in info['content']:
             self.world_losses += 1
             self.log('world lost')
 
@@ -211,9 +206,9 @@ class NMBot():
 
 
     # GENERAL
-    def check_energy():
+    def check_energy(self):
         # checks how much energy we have
-        response = self.session.get('http://www.ninjamanager.com')
+        response = self.session.get('https://www.ninjamanager.com', headers=self.headers, cookies=self.cookies)
         raw = response.text
 
         arena_energy_raw = raw[raw.find('js-header-energy-arena'):]
@@ -234,7 +229,7 @@ class NMBot():
 
 
 
-    def log(msg: str):
+    def log(self, msg: str):
         # prints to console and file
         print(msg)
         self.logger.write('\n' + msg)
@@ -244,12 +239,29 @@ class NMBot():
 
 
 def rng(start: float, stop:float) -> float:
+    # returns a random float between start and stop
     return uniform(start, stop)
 
 
 
+# def save_and_load(text):
+#     # saves and the immediately loads a json file because json.load() is funny
+#     with open('response.json', 'w+', encoding='utf-8') as file:
+#         file.write(text)
+
+#     with open('response.json', 'r', encoding='utf-8') as file:
+#         info = json.load(file)
+
+#     return info
+
 
 
 if __name__ == "__main__":
-    bot = NMBot()
+    with open('headers.json', 'r') as file:
+        headers = json.load(file)
+
+    with open('cookies.json', 'r') as file:
+        cookies = json.load(file)
+
+    bot = NMBot(headers, cookies)
     bot.execute()
